d557893f86cac6fe82f79924c0c2857b7da52c7a
Florian Vichot <florian.vichot@wifirst.fr>
2023-10-31 21:14:00 +1100

    Ship it

--- resume/scripts/render_yml.py
+++ resume/scripts/render_yml.py
@@ -5,7 +5,7 @@ import argparse
 from collections import OrderedDict
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from markupsafe import Markup, escape
-import toml
+import yaml
 import re
 
 LINKIFY_REGEX = re.compile('\[([^\]]+)\]\(([^\)]+)\)')
@@ -31,11 +31,15 @@ def filter_basename(value):
     return value if i == -1 else value[:i]
 
 
+def filter_newline(value):
+    return Markup(value.replace('\n', '<br>'))
+
+
 def global_icon(name, css_class="icon"):
     icon_data = ""
     with open('img/{}.svg'.format(name), 'rb') as file:
         icon_data = file.read().decode("utf-8")
-    return Markup(icon_data.replace("<svg", "<svg class={}".format(css_class)))
+    return Markup(icon_data.replace("<svg", "<svg class=\"{}\"".format(css_class)))
 
 def main():
     parser = argparse.ArgumentParser(description="Render a TOML file using "
@@ -47,7 +51,7 @@ def main():
     args = parser.parse_args()
 
     with open(args.input[0], 'rb') as f:
-        data = toml.loads(f.read().decode("utf-8"), _dict=OrderedDict)
+        data = yaml.safe_load(f.read().decode("utf-8"))
 
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['tmpl']))
@@ -55,6 +59,7 @@ def main():
     env.filters['bold'] = filter_bold
     env.filters['linkify'] = filter_linkify
     env.filters['basename'] = filter_basename
+    env.filters['newline'] = filter_newline
     env.globals["icon"] = global_icon
 
     template = env.get_template(args.template)
